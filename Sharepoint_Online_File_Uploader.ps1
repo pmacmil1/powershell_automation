0d7b510d71ff6e47cc262eea32641e71f5457d0f
Pierce MacMillan <pierce.macmillan@gmail.com>
2020-06-12 12:02:01 +0200

    Add files via upload

---
+++ Sharepoint_Online_File_Uploader.ps1
@@ -0,0 +1,159 @@
+﻿#You will need the Sharepoint Client CSOM Assembilies to run this code
+#The Windows installer can be found here: https://www.microsoft.com/en-us/download/details.aspx?id=42038
+
+Add-Type -Path "C:\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+
+Function Ensure-Folder()
+{
+Param(
+  [Parameter(Mandatory=$True)]
+  [Microsoft.SharePoint.Client.Web]$Web,
+
+  [Parameter(Mandatory=$True)]
+  [Microsoft.SharePoint.Client.Folder]$ParentFolder, 
+
+  [Parameter(Mandatory=$True)]
+  [String]$FolderUrl
+
+)
+    $folderNames = $FolderUrl.Trim().Split("/",[System.StringSplitOptions]::RemoveEmptyEntries)
+    $folderName = $folderNames[0]
+    Write-Host "Creating folder [$folderName] ..."
+    $curFolder = $ParentFolder.Folders.Add($folderName)
+    $Web.Context.Load($curFolder)
+    $web.Context.ExecuteQuery()
+    Write-Host "Folder [$folderName] has been created succesfully. Url: $($curFolder.ServerRelativeUrl)"
+
+    if ($folderNames.Length -gt 1)
+    {
+        $curFolderUrl = [System.String]::Join("/", $folderNames, 1, $folderNames.Length - 1)
+        Ensure-Folder -Web $Web -ParentFolder $curFolder -FolderUrl $curFolderUrl
+    }
+}
+
+
+Function Upload-File() 
+{
+Param(
+  [Parameter(Mandatory=$True)]
+  [Microsoft.SharePoint.Client.Web]$Web,
+
+  [Parameter(Mandatory=$True)]
+  [String]$FolderRelativeUrl, 
+
+  [Parameter(Mandatory=$True)]
+  [System.IO.FileInfo]$LocalFile
+
+)
+
+    try 
+    {
+       $FileExtension = [System.IO.Path]::GetExtension($LocalFile.Name)
+	   #Confirm the file type, that you want to upload here
+	   #Currently set for PDFs
+       if ($FileExtension -eq ".pdf")
+       {
+           
+           $fileUrl = $FolderRelativeUrl + "/" + $LocalFile.Name
+           Write-Host "Uploading file [$($LocalFile.FullName)] ... to $fileUrl"
+           [Microsoft.SharePoint.Client.File]::SaveBinaryDirect($Web.Context, $fileUrl, $LocalFile.OpenRead(), $true)
+           Write-Host "File [$($LocalFile.FullName)] has been uploaded succesfully. Url: $fileUrl"
+       }
+       else
+       {
+         write-host "This file is not a pdf [$($LocalFile.FullName)]"
+       }
+    }
+    catch 
+    {
+       write-host "An error occured while uploading file [$($LocalFile.FullName)]"
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName
+        $ErrorMessage
+        $FailedItem
+    }
+}
+
+
+function Upload-Files()
+{
+
+Param(
+  [Parameter(Mandatory=$True)]
+  [String]$Url,
+
+  [Parameter(Mandatory=$True)]
+  [String]$UserName,
+
+  [Parameter(Mandatory=$False)]
+  [String]$Password, 
+
+  [Parameter(Mandatory=$True)]
+  [String]$TargetListTitle,
+
+  [Parameter(Mandatory=$True)]
+  [String]$SourceFolderPath
+
+)
+
+    if($Password) {
+       $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+    }
+    else {
+      $SecurePassword = Read-Host -Prompt "Enter the password" -AsSecureString
+    }
+    $Context = New-Object Microsoft.SharePoint.Client.ClientContext($Url)
+    $Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($UserName,$SecurePassword)
+    $Context.Credentials = $Credentials
+    $web = $Context.Web 
+    $Context.Load($web)
+    $list = $web.Lists.GetByTitle($TargetListTitle);
+    $Context.Load($list.RootFolder)
+    $Context.ExecuteQuery()
+
+
+    $Date = (get-date).ToString(‘dd-MM-yy’)
+
+    
+    $curFolder = $list.RootFolder.Folders.Add($Date)
+    $Web.Context.Load($curFolder)
+    $web.Context.ExecuteQuery()
+
+    $folderRelativeUrl = $folderRelativeUrl + "/"+ $Date
+    $DateFolder = $curFolder
+    $DateFolder = [Microsoft.SharePoint.Client.Folder]$DateFolder
+
+    Get-ChildItem $SourceFolderPath -Recurse | % {
+       if ($_.PSIsContainer -eq $True) {
+          $folderUrl = $_.FullName.Replace($SourceFolderPath,"").Replace("\","/")   
+          if($folderUrl) {
+             Ensure-Folder -Web $web -ParentFolder $DateFolder -FolderUrl $folderUrl
+          }
+       }
+       else{
+          $Date = (get-date).ToString(‘dd-MM-yy’)
+          $folderRelativeUrl = $list.RootFolder.ServerRelativeUrl + "/" + $Date + $_.DirectoryName.Replace($SourceFolderPath,"").Replace("\","/")
+          $folderRelativeUrl
+          Upload-File -Web $web -FolderRelativeUrl $folderRelativeUrl -LocalFile $_ 
+       }
+    }
+}
+
+#Usage
+$LogfileDate = (get-date).ToString(‘d-M-y’)
+#Change the directory for the log file
+$Logfile = "\\FILESHARE\Sharepoint Uploader\SharepointUploader - "+$LogfileDate+".log"
+Start-Transcript $LogFile
+
+$Url = "https://DOMAIN_NAME.sharepoint.com/sites/SITE_NAME/"
+
+$TargetListTitle = "TARGET"  #Target Library
+
+
+$SourceFolderPath = "C:\FILES_TO_UPLOAD"  #Source Physical Path 
+
+#Upload files
+Upload-Files -Url $Url -UserName $UserName -Password $Password -TargetListTitle $TargetListTitle -SourceFolderPath $SourceFolderPath
+
+Stop-Transcript
---
+++ Update_Sharepoint_Online_User_Info.ps1
@@ -0,0 +1,92 @@
+﻿#You will need the Sharepoint Client CSOM Assembilies to run this code
+#The Windows installer can be found here: https://www.microsoft.com/en-us/download/details.aspx?id=42038
+
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.UserProfiles.dll"
+$Cred= Get-Credential
+
+Function Update-UserProfileProperty()
+{
+    param
+    (
+        [Parameter(Mandatory=$true)] [string] $AdminCenterURL,
+        [Parameter(Mandatory=$true)] [string] $UserAccount,
+        [Parameter(Mandatory=$true)] [string] $PropertyName,
+        [Parameter(Mandatory=$true)] [string] $PropertyValue
+    )    
+    Try {
+        #Setup Credentials to connect
+        
+        $Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Cred.Username, $Cred.Password)
+ 
+        #Setup the context
+        $Ctx = New-Object Microsoft.SharePoint.Client.ClientContext($AdminCenterURL)
+        $Ctx.Credentials = $Credentials
+         
+        #Get the User
+        $User = $Ctx.web.EnsureUser($UserAccount)
+        $Ctx.Load($User)
+        $Ctx.ExecuteQuery()
+ 
+        #Get User Profile
+        $PeopleManager = New-Object Microsoft.SharePoint.Client.UserProfiles.PeopleManager($Ctx)
+         
+        #update User Profile Property
+        $PeopleManager.SetSingleValueProfileProperty($User.LoginName, $PropertyName, $PropertyValue)
+        $Ctx.ExecuteQuery()
+ 
+        #Write-host "User Profile Property has been Updated!" -f Green
+    }
+    Catch {
+        write-host -f Red "Error Updating User Profile Property!" $_.Exception.Message
+    }
+}
+
+#Define Parameter values
+#NOTE: YOU HAVE TO USE THE ADMIN PAGE HERE, NOT THE REGULAR SHAREPOINT ONLINE URL FOR YOUR TENANT!
+$AdminCenterURL="https://TENANT_NAME-admin.sharepoint.com"
+$SipAddress = 'SPS-SipAddress'
+$WorkPhone = 'WorkPhone'
+$MobilePhone = 'CellPhone'
+
+$Users = Import-CSV -Delimiter ';' -Path "C:\PATH_TO_CSV\SOME_CSV.CSV" -Encoding utf8
+
+ForEach($User in $Users)
+{
+    #'Updateing User: '+$User.UserPrincipalName+' with extension number: '+$User.extensionNumber
+    $SPUserName = 'i:0#.f|membership|'+$User.UserPrincipalName
+    
+    if($user.extensionNumber -eq "")
+    {
+        'Updateing User: '+ $SPUserName + ' with the SipAddress of ' + '-'
+        Update-UserProfileProperty -AdminCenterURL $AdminCenterURL -UserAccount $SPUserName -PropertyName $SipAddress -PropertyValue '-'
+    }
+    else
+    {
+        'Updateing User: '+ $SPUserName + ' with the SipAddress of ' + $user.extensionNumber
+        Update-UserProfileProperty -AdminCenterURL $AdminCenterURL -UserAccount $SPUserName -PropertyName $SipAddress -PropertyValue $User.extensionNumber
+    }
+
+    if($user.NewPhoneNumber -eq "")
+    {
+        'Updateing User: '+ $SPUserName + ' with the NewPhoneNumber of ' + '-'
+        Update-UserProfileProperty -AdminCenterURL $AdminCenterURL -UserAccount $SPUserName -PropertyName $WorkPhone -PropertyValue '-'
+    }
+    else
+    {
+        'Updateing User: '+ $SPUserName + ' with the NewPhoneNumber of ' + $user.NewPhoneNumber
+        Update-UserProfileProperty -AdminCenterURL $AdminCenterURL -UserAccount $SPUserName -PropertyName $WorkPhone -PropertyValue $User.NewPhoneNumber
+    }
+
+    if($user.MobilePhone -eq "")
+    {
+        'Updateing User: '+ $SPUserName + ' with the MobilePhone of ' + '-'
+        Update-UserProfileProperty -AdminCenterURL $AdminCenterURL -UserAccount $SPUserName -PropertyName $MobilePhone -PropertyValue '-'
+    }
+    else
+    {
+        'Updateing User: '+ $SPUserName + ' with the MobilePhone of ' + $user.MobilePhone
+        Update-UserProfileProperty -AdminCenterURL $AdminCenterURL -UserAccount $SPUserName -PropertyName $MobilePhone -PropertyValue $User.MobilePhone
+    }
+}
